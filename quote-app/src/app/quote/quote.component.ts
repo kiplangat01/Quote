4a3fb29ad5a712062b3e6644180f3b0a10485d79
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-04-09 21:57:33 +0300

    added quote arrays

--- quote-app/src/app/quote/quote.component.ts
+++ quote-app/src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
-  constructor() { }
+  quote: Quote[] = [
+    
+  new Quote (1,'Jared','Living','The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela', new Date(2019,7,12),0,0),
+  new Quote (2,'Zimmer man','Hardwork','The way to get started is to quit talking and begin doing.','Walt Disney',new Date(2019,7,14),0,0),
+  ]
+  get myQuotes(){
+    return this.quote.sort((a,b)=>{
+      return <any>new Date(b.datePosted) - <any>new Date(a.datePosted);
+    });
+  }
+  
+    constructor() { }  
+  
 
   ngOnInit(): void {
   }

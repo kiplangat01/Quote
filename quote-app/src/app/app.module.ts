bac9c9349cc91d40cc2acd9660036034048ebdb8
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-04-09 13:42:54 +0300

    added bootstrap file

---
+++ quote-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuoteComponent,
+    QuoteFormComponent,
+    QuoteDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

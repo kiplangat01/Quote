622e488cba204a002d711437fac4514d560e8372
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-04-11 23:14:45 +0300

    made vote buttons functional

--- quote-app/src/app/quote/quote.component.ts
+++ quote-app/src/app/quote/quote.component.ts
@@ -10,8 +10,8 @@ export class QuoteComponent implements OnInit {
 
   quote: Quote[] = [
     
-  new Quote (1,'mandela','hope','May your choices reflect your hopes, not your fears.','Nelson Mandela', new Date(2019,7,12),0,0),
-  new Quote (2,'obama','Hardwork','We did not come to fear the future. We came here to shape it.','kiplangat meshack',new Date(2019,7,14),0,0),
+  new Quote (1,'mandela','hope','Taking responsibility - practicing 100 percent responsibility every day - is about seeing ourselves not as right or wrong, but as an agent, chooser, problem solver, and learner in the complex interrelationships of our lives so that we can better integrate with the people and world around us. When we do this, we enjoy a better and more productive way to live and lead."','Nelson Mandela', new Date(2019,7,12),0,0),
+  new Quote (2,'obama','Hardwork','Most of those who have succeeded in life can trace their success back to the essential education they obtained from parents, teachers and/ or friends','kiplangat meshack',new Date(2019,7,14),0,0),
   ]
   get myQuotes(){
  
